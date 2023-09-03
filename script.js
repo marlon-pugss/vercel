335e1eb13b9b19126db15476d2136af402ddcb3b
marlon-pugss <pugssmarlon@gmail.com>
2023-09-03 20:19:56 -0300

    commit5

--- script.js
+++ script.js
@@ -1,28 +1,34 @@
-let expressao = '';
-const display = document.getElementById('display');
+document.addEventListener("DOMContentLoaded", function () {
+    const cepInput = document.getElementById("cepInput");
+    const buscarButton = document.getElementById("buscarButton");
+    const resultado = document.getElementById("resultado");
 
-function adicionarNumero(numero) {
-    expressao += numero;
-    atualizarDisplay();
-}
+    buscarButton.addEventListener("click", function () {
+        const cep = cepInput.value.replace(/\D/g, "");
 
-function calcular() {
-    try {
-        const resultado = eval(expressao);
-        expressao = resultado.toString();
-        atualizarDisplay();
-    } catch (error) {
-        expressao = '';
-        atualizarDisplay();
-        alert('Erro ao calcular a expressão.');
-    }
-}
+        if (cep.length !== 8) {
+            resultado.textContent = "O CEP deve conter 8 dígitos.";
+            return;
+        }
 
-function limpar() {
-    expressao = '';
-    atualizarDisplay();
-}
-
-function atualizarDisplay() {
-    display.value = expressao;
-}
+        fetch(`https://viacep.com.br/ws/${cep}/json/`)
+            .then(response => response.json())
+            .then(data => {
+                if (data.erro) {
+                    resultado.textContent = "CEP não encontrado.";
+                } else {
+                    resultado.innerHTML = `
+                        <h2>Resultado da busca:</h2>
+                        <p><strong>CEP:</strong> ${data.cep}</p>
+                        <p><strong>Logradouro:</strong> ${data.logradouro}</p>
+                        <p><strong>Bairro:</strong> ${data.bairro}</p>
+                        <p><strong>Cidade:</strong> ${data.localidade}</p>
+                        <p><strong>Estado:</strong> ${data.uf}</p>
+                    `;
+                }
+            })
+            .catch(error => {
+                resultado.textContent = "Ocorreu um erro ao buscar o CEP.";
+            });
+    });
+});
